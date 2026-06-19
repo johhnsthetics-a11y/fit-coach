262671a2ad996c925b7138d2584ff22d46dbfd2b
johhnsthetics-a11y <johhnsthetics@gmail.com>
2026-06-19 16:06:01 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
